4c0e361a0de405e5a1e221cc58171b6c8b273f41
dream <xm@dqgs.fun>
2019-07-01 14:14:14 +0800

    config

--- config-server/src/main/java/fun/dqgs/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/fun/dqgs/configserver/ConfigServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer     //激活对配置中心的支持
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
